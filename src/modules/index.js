3ee50f8e9771435d508411cba11d0b80a5215a3e
Adam Morris <amorris@mistermorris.com>
2021-02-21 10:59:36 +0800

    initial

---
+++ src/modules/index.js
@@ -0,0 +1,10 @@
+/*
+
+This directory (src/modules/) is where you can write appsscript function, classes, etc, and export them, just like with es modules.
+
+When the command `npm run build` is executed, all of the code in this directory
+is put into build/Bundle.js
+
+ */
+
+export const anything = {};
